94e63ef4cc5cb57b1ec8d0c1a7c2cca14edba9e8
Arnav Srivastava <sriva128@ececomp2.ecn.purdue.edu>
2024-07-15 21:29:25 -0400

    DONE

--- Collision-Detector.c
+++ Collision-Detector.c
@@ -2,109 +2,74 @@
 #include <stdlib.h>
 #include <math.h>
 
-struct Point {
+struct Point
+{
     int x;
     int y;
 };
 
-struct KDNode {
-    struct Point point;
-    struct KDNode *left;
-    struct KDNode *right;
-};
-
-double squared_distance(int x1, int y1, int x2, int y2) {
-    return(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-}
-
-struct KDNode* insert(struct KDNode *root, struct Point point, int depth) {
-    if (root == NULL) {
-        struct KDNode *node = (struct KDNode *)malloc(sizeof(struct KDNode));
-        node->point = point;
-        node->left = node->right = NULL;
-        return node;
-    }
-
-    int cd = depth % 2; // Current dimension (0 for x, 1 for y)
-
-    if (cd == 0) {
-        if (point.x < root->point.x)
-            root->left = insert(root->left, point, depth + 1);
-        else
-            root->right = insert(root->right, point, depth + 1);
-    } else {
-        if (point.y < root->point.y)
-            root->left = insert(root->left, point, depth + 1);
-        else
-            root->right = insert(root->right, point, depth + 1);
-    }
-
-    return root;
-}
-
-void search(struct KDNode *root, struct Point query, int r_squared, int depth, int *collision_count) {
-    if (root == NULL) return;
-
-    if (squared_distance(root->point.x, root->point.y, query.x, query.y) <= r_squared)
-        (*collision_count)++;
-
-    int cd = depth % 2;
-
-    if (cd == 0) {
-        if ((query.x - r_squared <= root->point.x) && (query.x + r_squared >= root->point.x)) {
-            search(root->left, query, r_squared, depth + 1, collision_count);
-            search(root->right, query, r_squared, depth + 1, collision_count);
-        } else if (query.x < root->point.x)
-            search(root->left, query, r_squared, depth + 1, collision_count);
-        else
-            search(root->right, query, r_squared, depth + 1, collision_count);
-    } else {
-        if ((query.y - r_squared <= root->point.y) && (query.y + r_squared >= root->point.y)) {
-            search(root->left, query, r_squared, depth + 1, collision_count);
-            search(root->right, query, r_squared, depth + 1, collision_count);
-        } else if (query.y < root->point.y)
-            search(root->left, query, r_squared, depth + 1, collision_count);
-        else
-            search(root->right, query, r_squared, depth + 1, collision_count);
-    }
-}
-
-void free_tree(struct KDNode *root) {
-    if (root == NULL) return;
-    free_tree(root->left);
-    free_tree(root->right);
-    free(root);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("ERROR! Argument count not equal to 2\n");
+struct Point* read_points(const char* filename, int* num_points)
+{
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        perror("Error opening file");
         return(1);
     }
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("Error opening file: %s\n", argv[1]);
-        return(1);
-    }
+    int capacity = 10;
+    *num_points = 0;
+    struct Point* points = (struct Point*)malloc(capacity * sizeof(struct Point));
 
-    struct KDNode *root = NULL;
-    struct Point point;
-    while (fscanf(file, "%d %d", &point.x, &point.y) == 2) {
-        root = insert(root, point, 0);
+    while (fscanf(file, "%d %d", &points[*num_points].x, &points[*num_points].y) == 2)
+    {
+        (*num_points)++;
+        if (*num_points >= capacity)
+        {
+            capacity *= 2;
+            points = (struct Point*)realloc(points, capacity * sizeof(struct Point));
+        }
     }
 
     fclose(file);
+    return(points);
+}
 
-    int query_x, query_y, query_r;
-    while (scanf("%d %d %d", &query_x, &query_y, &query_r) == 3) {
-        struct Point query = {query_x, query_y};
-        int collision_count = 0;
-        int r_squared = query_r * query_r;
-        search(root, query, r_squared, 0, &collision_count);
-        printf("%d\n", collision_count);
+int count_points_within_radius(struct Point* points, int num_points, int x_center, int y_center, int radius)
+{
+    int count = 0;
+    int radius_squared = radius * radius;
+
+    for (int i = 0; i < num_points; i++)
+    {
+        int dx = points[i].x - x_center;
+        int dy = points[i].y - y_center;
+        if (dx * dx + dy * dy <= radius_squared)
+        {
+            count++;
+        }
+    }
+    return(count);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <points file>\n", argv[0]);
+        return(1);
     }
 
-    free_tree(root);
+    int num_points;
+    struct Point* points = read_points(argv[1], &num_points);
+
+    int x_center, y_center, radius;
+    while (scanf("%d %d %d", &x_center, &y_center, &radius) == 3)
+    {
+        int count = count_points_within_radius(points, num_points, x_center, y_center, radius);
+        printf("%d\n", count);
+    }
+
+    free(points);
     return(0);
 }
